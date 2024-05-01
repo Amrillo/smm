2f0650a924496c7376d91c564c349d403f730ed6
Amrillo <amrishka88@gmail.com>
2024-05-01 22:55:46 +0500

    do layout for webblog

--- src/app/features/blog/blog-routing.module.ts
+++ src/app/features/blog/blog-routing.module.ts
@@ -5,7 +5,7 @@ import { ArticleComponent } from './article/article.component';
 
 const routes: Routes = [
   {path: 'blog', component:  WeblogComponent},
-  {path: 'article/:', component:  ArticleComponent},
+  {path: 'article/:id', component:  ArticleComponent},
 ];
 
 @NgModule({
