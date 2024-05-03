09b0f9da0e1db0609daf1b67dd9c5a7998fce3df
Amrillo <amrishka88@gmail.com>
2024-05-03 18:13:29 +0500

    add codes to webblog

--- src/app/features/blog/weblog/weblog.component.ts
+++ src/app/features/blog/weblog/weblog.component.ts
@@ -1,10 +1,11 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { throwError } from 'rxjs';
+import { Router } from '@angular/router';
 import { ArticleService } from 'src/app/shared/services/article.service';
 import { CategoryService } from 'src/app/shared/services/category.service';
-import { ArticlesAllType } from 'src/types/articles-All.type';
+import { ActiveParamsType } from 'src/types/active-params.type';
+import { ArticlesAllType } from 'src/types/articles-all.type';
 import { ArticlesType } from 'src/types/articles.type';
 import { CategoryType } from 'src/types/category.type';
 
@@ -18,9 +19,12 @@ export class WeblogComponent implements OnInit {
   sortingOpen: boolean = false;
   activeIndex: number = -1;
   categoryTypes: CategoryType[] = [];
+  activeParams: ActiveParamsType | null  = null; 
   articles: ArticlesType[] = [];
-
-  constructor(private categoryS: CategoryService, private articlesS: ArticleService, private _snackBar: MatSnackBar) { }
+  pages: number[] = []; 
+  activePage: number = 1 ; 
+  constructor(private categoryS: CategoryService, private articlesS: ArticleService, 
+    private _snackBar: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void {
 
@@ -30,18 +34,25 @@ export class WeblogComponent implements OnInit {
           this.activeIndex = -1;
       }) // получения всех категорий
 
-      this.articlesS.getAllArticles()
-       .subscribe( {
-          next:  (data:ArticlesAllType)=> {
-              if(data !== undefined) {
-                this.articles = data.items;
-              }
-          },
-          error: (errorResponse: HttpErrorResponse)=> {
-             this._snackBar.open(errorResponse.error);
-          }
-        }
-       )
+      // this.articlesS.getAllArticles()
+      //  .subscribe( {
+      //     next:  (data:ArticlesAllType)=> {
+      //         if(data !== undefined) {
+      //           this.articles = data.items;
+      //           console.log(data);
+      //           this.pages = [];
+          
+      //           for(let i = 1; i <= data.pages; i++ ){ 
+      //             this.pages.push(i); 
+      //           }
+      //           this.activePage = 1; 
+      //         }
+      //     },
+      //     error: (errorResponse: HttpErrorResponse)=> {
+      //        this._snackBar.open(errorResponse.error);
+      //     }
+      //   }
+      //  )
   }
 
   toggleSorting():void {
@@ -49,15 +60,38 @@ export class WeblogComponent implements OnInit {
     // отритыя выпадающего списка
   }
 
-  toggleActive(index: number) {
-      // Check if the clicked index is the same as the currently active index
-    if (index === this.activeIndex) {
-      // If it is, set the active index to -1 (no item active)
-      this.activeIndex = -1;
-  } else {
-      // If it's different, set the active index to the clicked index
-      this.activeIndex = index;
+  toggleActive(index: number, category:CategoryType):void {
+        // Check if the clicked index is the same as the currently active index
+      if (index === this.activeIndex) {
+        // If it is, set the active index to -1 (no item active)
+        this.activeIndex = -1;
+        console.log("grabbed")
+    } else {
+        // If it's different, set the active index to the clicked index
+        this.activeIndex = index;
+       
+          this.activeParams?.page = index;
+          this.router.navigate(['/blog'], { 
+            queryParams:  this.activeParams 
+          })
+          console.log('yes')
+        
+       
+        console.log(this.activeParams);
+    }
   }
+    selectPage(page: number):void {  
+      this.activePage = page;   
   }
 
+  goToPrev():void {  
+    if(this.activePage !== 1) {  
+      this.activePage--
+    }
+  }
+    goToNext():void {  
+      if(this.activePage !== this.pages.length ) { 
+        this.activePage++
+      }
+    }
 }
--- src/app/shared/services/article.service.ts
+++ src/app/shared/services/article.service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ArticlesAllType } from 'src/types/articles-All.type';
+import { ActiveParamsType } from 'src/types/active-params.type';
+import { ArticlesAllType } from 'src/types/articles-all.type';
 import { ArticlesType } from 'src/types/articles.type';
 
 @Injectable({
@@ -16,8 +17,10 @@ export class ArticleService {
         return this.http.get<ArticlesType[]>(environment.api + 'articles/top');
     }
 
-    getAllArticles(): Observable<ArticlesAllType> {   
-       return this.http.get<ArticlesAllType>(environment.api + 'articles');
+    getAllArticles(params: ActiveParamsType): Observable<ArticlesAllType> {   
+       return this.http.get<ArticlesAllType>(environment.api + 'articles', {   
+        params: params
+       });
     }
 
 }
